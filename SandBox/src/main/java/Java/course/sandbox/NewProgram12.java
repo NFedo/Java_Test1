129fb24cbbbfab2f02e01cce3a4ba2f5a8b11553
Nadezda Fedorova <Nadejda.Fedorova@billing.ru>
2016-04-13 16:08:19 +0300

    Переименован пакет из Java.course в javaCourse

--- SandBox/src/main/java/Java/course/sandbox/NewProgram12.java
+++
@@ -1,7 +0,0 @@
-package Java.course.sandbox;
-
-public class NewProgram12 {
-	public static void main(String[] args){
-	    System.out.println("Hello, world");
-	}
-}
---
+++ SandBox/src/main/java/javaCourse/sandbox/NewProgram12.java
@@ -0,0 +1,7 @@
+package javaCourse.sandbox;
+
+public class NewProgram12 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
