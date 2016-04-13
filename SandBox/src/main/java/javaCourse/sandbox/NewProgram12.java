dd6d874252b53b10d5b438826fd336148617ab88
Nadezda Fedorova <Nadejda.Fedorova@billing.ru>
2016-04-13 22:39:04 +0300

    Добавлены функции

--- SandBox/src/main/java/javaCourse/sandbox/NewProgram12.java
+++ SandBox/src/main/java/javaCourse/sandbox/NewProgram12.java
@@ -2,6 +2,35 @@ package javaCourse.sandbox;
 
 public class NewProgram12 {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        String somebody = "world";
+        System.out.println("Hello, " + somebody + "!");
+
+        hello("world !");
+        hello("world !!");
+        hello("world !!!");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+
+        int Lo = 6;
+        double ll  = 8.0;
+        int s = Lo * Lo;
+        System.out.println("Площадь квадрата со стороной " + Lo + " = " + (Lo * Lo));
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) { // совсем другая переменная l
+      return len * len ;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
