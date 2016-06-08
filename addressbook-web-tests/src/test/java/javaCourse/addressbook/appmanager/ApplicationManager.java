6abf8ad619ccb155fc99d80ffe2e4e4f0c308ab3
Nadezda Fedorova <Nadejda.Fedorova@billing.ru>
2016-06-08 21:11:11 +0300

    Создан конфигурационный файл для стенда, добавлено логирование
    Создан конфигурационный файл для стенда, добавлено логирование

--- addressbook-web-tests/src/test/java/javaCourse/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/javaCourse/addressbook/appmanager/ApplicationManager.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,24 +25,36 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {   this.browser = browser;  }
+  private final Properties properties;
 
-  public void init() {
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     // String browser = BrowserType.FIREFOX; // BrowserType входит в Selenium
-    if (browser == BrowserType.FIREFOX) {
-      wd = new ChromeDriver();
-    } else if (browser == BrowserType.CHROME) {
+    // System.out.println(BrowserType.FIREFOX);
+    // System.out.println(BrowserType.CHROME);
+    // System.out.println(BrowserType.IE);
+
+    if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.IE) {
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook");
+    // wd.get("http://localhost/addressbook");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
